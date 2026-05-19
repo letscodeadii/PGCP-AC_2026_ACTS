1771d326a9430b48d8c16cf9a83ddbc9b9d4cba8
letscodeadii <letsmailadii@gmail.com>
2026-05-19 20:24:31 +0530

    First

---
+++ CMCE/Core_Java/Day 1/src/sum.java
@@ -0,0 +1,7 @@
+public class sum {
+    public static void main(String[] args){
+        double num1=Double.parseDouble(args[0]);
+        double num2=Double.parseDouble(args[1]);
+        System.out.println("Sum "+ (num1+num2));
+    }
+}
