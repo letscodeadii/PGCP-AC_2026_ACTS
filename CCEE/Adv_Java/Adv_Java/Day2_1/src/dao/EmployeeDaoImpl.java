961954434f70384259f14853ab59b0f56865f727
letscodeadii <letsmailadii@gmail.com>
2026-05-27 20:30:14 +0530

    POJOS

--- CCEE/Adv_Java/Adv_Java/Day2_1/src/dao/EmployeeDaoImpl.java
+++ CCEE/Adv_Java/Adv_Java/Day2_1/src/dao/EmployeeDaoImpl.java
@@ -1,16 +1,30 @@
 package dao;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
-
 import pojos.Employee;
+import static utils.DBUtils.openConnection;
 
 public class EmployeeDaoImpl implements IEmployeeDao {
+	//STATE :instance vars
+	private Connection cn;
+	private PreparedStatement pst1;
+	
+	public EmployeeDaoImp() throws SQLException {
+		//get fix db connection from DBUtils
+		cn=openConnection();
+		pst1=cn.prepareStatement("sle");
+	}
+	
+	
+	
 
 	@Override
-	public List<Employee>employees getSelectedEmpDetails(String dept, Date beginDate, Date endDate) throws SQLException {
-		// TODO Auto-generated method stub
+	public List<Employee> getSelectedEmpDetails(String dept, Date beginDate, Date endDate) throws SQLException {
+		// TODO Auto-generated method stub 
 		return null;
 	}
 
