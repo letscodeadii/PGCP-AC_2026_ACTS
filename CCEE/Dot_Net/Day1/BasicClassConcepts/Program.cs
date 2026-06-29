3cfebb29ef9410db32c578e30b595aeee231291a
letscodeadii <letsmailadii@gmail.com>
2026-06-29 19:26:51 +0530

    .net_day01

---
+++ CCEE/Dot_Net/Day1/BasicClassConcepts/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassConcepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Console.WriteLine("Hello World");
+
+            Console.WriteLine("Hello World ");
+            Console.ReadLine();
+
+            //n1.Class1
+            //n1.n1a.Class3
+        }
+        
+    }
+
+}
+namespace n1
+{
+    public class Class1 { }
+    public class Class2 { }
+    
+    
+    namespace n1a
+    {
+        public class Class3 { } 
+    }
+
+}
